47d0a661b183d8256692512b587a4c072eec7049
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-09-14 00:06:28 +0530

    fetch api and error handling

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const inputText = document.getElementById("input-textarea")
 const translateBtn = document.getElementById("translate-btn")
 const outputText = document.getElementById("output-text")
+
+// Event listener
+
+translateBtn.addEventListener('click', () => {
+    if(inputText.value != "") {
+        fetch(`https://api.funtranslations.com/translate/minion.json?text=${inputText.value}`)
+        .then(res => res.json())
+        .then(data => {
+            if(data.contents) outputText.innerText = data.contents.translated
+            else alert(data.error.message)
+        })
+        .catch(err => {
+            alert(err)
+            console.log("error ", err)
+        })
+    }   
+})
